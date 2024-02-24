17c2551137c450300c939503a76cb363c58ad3d7
NekruzAsh <nekruzashrapov16@gmail.com>
2024-02-24 18:52:58 -0500

    cleared page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,6 @@ import "./globals.css";
 
 export default function Home() {
   return (
-    <div className="items-center flex justify-center max-w-[1280px]">
-      
-    </div>
+    <div></div>
   );
 }
