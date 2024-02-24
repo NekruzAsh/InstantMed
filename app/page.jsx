bd7a88ecb394e3ecb858037689d477515e049736
NekruzAsh <nekruzashrapov16@gmail.com>
2024-02-24 18:48:46 -0500

    add this file.

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import "./globals.css";
+
+
+export default function Home() {
+  return (
+    <div className="items-center flex justify-center max-w-[1280px]">
+      
+    </div>
+  );
+}
